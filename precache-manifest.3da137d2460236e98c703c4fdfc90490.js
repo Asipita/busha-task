e404168e759d08be691a8f432c83d9d74ccbccd9
Abdulgafar <ashuaib@healthstation.ng>
2020-09-08 16:38:58 +0100

    Updates

---
+++ precache-manifest.3da137d2460236e98c703c4fdfc90490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904f204761547f3881be32f83e7c3129",
+    "url": "/busha-task/index.html"
+  },
+  {
+    "revision": "d2efdf9b9b84a79ddcbd",
+    "url": "/busha-task/static/css/main.f4fef0e2.chunk.css"
+  },
+  {
+    "revision": "d88c4826518dc36f453c",
+    "url": "/busha-task/static/js/2.2cc4959e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/busha-task/static/js/2.2cc4959e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2efdf9b9b84a79ddcbd",
+    "url": "/busha-task/static/js/main.39b720cd.chunk.js"
+  },
+  {
+    "revision": "16430c93fbc6d826d253",
+    "url": "/busha-task/static/js/runtime-main.8c59f2dc.js"
+  }
+]);
